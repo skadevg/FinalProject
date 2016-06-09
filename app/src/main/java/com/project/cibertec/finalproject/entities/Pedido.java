6c4735f89ab79c1b1d7b64f01a16f7a4f797346b
Jjaulis <jorge.jaulisrua@one.verizon.com>
2016-06-08 22:00:42 -0500

    Add new SQL Database and Interfaces

--- app/src/main/java/com/project/cibertec/finalproject/entities/Pedido.java
+++ app/src/main/java/com/project/cibertec/finalproject/entities/Pedido.java
@@ -1,15 +1,22 @@
 package com.project.cibertec.finalproject.entities;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
-public class Pedido {
+public class Pedido implements Parcelable  {
 
     private int idPedido;
     private int idCliente;
     private String nombreCliente;
     private int cantidadProductos;
     private double totalPedido;
+    private double precio ;
     private ArrayList<PedidoDetalle> lineas;
+    private String empresa;
+
+    public  Pedido (){}
 
     public int getIdPedido() {
         return idPedido;
@@ -43,12 +50,28 @@ public class Pedido {
         this.nombreCliente = nombreCliente;
     }
 
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
     public double getTotalPedido() {
         return totalPedido;
     }
 
     public void setTotalPedido(double totalPedido) {
         this.totalPedido = totalPedido;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
     }
 
     public ArrayList<PedidoDetalle> getLineas() {
@@ -58,4 +81,46 @@ public class Pedido {
     public void setLineas(ArrayList<PedidoDetalle> lineas) {
         this.lineas = lineas;
     }
+
+
+
+    protected Pedido(Parcel in) {
+        idPedido = in.readInt();
+        idCliente = in.readInt();
+        nombreCliente = in.readString();
+        cantidadProductos = in.readInt();
+        empresa = in.readString();
+        totalPedido = in.readDouble();
+        precio = in.readDouble();
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(idPedido);
+        dest.writeInt(idCliente);
+        dest.writeString(nombreCliente);
+        dest.writeInt(cantidadProductos);
+        dest.writeString(empresa);
+        dest.writeDouble(totalPedido);
+        dest.writeDouble(precio);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Pedido> CREATOR = new Parcelable.Creator<Pedido>() {
+        @Override
+        public Pedido createFromParcel(Parcel in) {
+            return new Pedido(in);
+        }
+
+        @Override
+        public Pedido[] newArray(int size) {
+            return new Pedido[size];
+        }
+    };
 }
